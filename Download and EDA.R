c53a6285b57672b3a58d5c2f5b99479df06b9464
Kaixiang Loke <kaixiangloke@Kaixiangs-MacBook-Air-3.local>
2025-11-01 21:56:09 -0400

    Analyzed median of returns after time_forward days across mid, low, and high

--- Download and EDA.R
+++ Download and EDA.R
@@ -13,7 +13,7 @@ spy$timestamp <- ymd_hms(spy$timestamp, tz = "UTC")
 spy$timestamp <- with_tz(spy$timestamp, tzone = "America/New_York")
 
 spy = spy |>
-  mutate(RSI = RSI(close, n = 20))
+  mutate(RSI = RSI(close, n = 14))
 
 dim(spy)
 summary(spy)
@@ -44,10 +44,10 @@ spy = spy |>
 
 filter(spy, RSI < 20)
 
-p1 = ggplot(filter(spy, symbol == "ABBV"), aes(timestamp, open)) + 
+p1 = ggplot(filter(spy, symbol == "AAPL"), aes(timestamp, open)) + 
   geom_line()
 
-p2 = ggplot(filter(spy, symbol == "ABBV"), aes(timestamp, RSI)) + 
+p2 = ggplot(filter(spy, symbol == "AAPL"), aes(timestamp, RSI)) + 
   geom_line()
 
 p1/p2
@@ -68,26 +68,50 @@ low_RSI = spy |>
   ) |>
   filter(RSI < 30 & RSI > 20)
 
+time_forward = 50
+
+#future look
+
+mean(spy$close)
+
 future_RSI_low = spy |>
+  group_by(symbol) |>
   mutate(
-    forward = lead(change, 20)
+    forward = lead(close, time_forward)/close
   ) |>
   filter(RSI < 30 & RSI > 20)
 
 future_RSI_mid = spy |>
+  group_by(symbol) |>
   mutate(
-    forward = lead(change, 20)
+    forward = lead(close, time_forward)/close
   ) |>
   filter(RSI < 60 & RSI > 40)
 
+future_RSI_high = spy |>
+  group_by(symbol) |>
+  mutate(
+    forward = lead(close, time_forward)/close
+  ) |>
+  filter(RSI > 70 & RSI < 80)
+
 ggplot(mid_RSI, aes(change)) + 
-  geom_histogram(bins = 50)
+  geom_histogram(bins = 50) + 
+  labs(
+    title = "Average Change on Days with Low RSI"
+  )
 
 ggplot(mid_RSI, aes(forward)) + 
-  geom_histogram(bins = 50)
+  geom_histogram(bins = 50) + 
+  labs(
+    title = "Average Change the Day After a Day with Low RSI"
+  )
 
 ggplot(mid_RSI, aes(backward)) + 
-  geom_histogram(bins = 50)
+  geom_histogram(bins = 50) + 
+  labs(
+    title = "Average Change the Day Before a Day with Low RSI"
+  )
 
 ggplot(low_RSI, aes(change)) + 
   geom_histogram(bins = 50)
@@ -101,10 +125,14 @@ ggplot(future_RSI_low, aes(forward)) +
 ggplot(future_RSI_mid, aes(forward)) + 
   geom_histogram(bins = 50)
 
+ggplot(future_RSI_high, aes(forward)) + 
+  geom_histogram(bins = 50)
+
 ggplot(spy, aes(RSI)) + 
   geom_histogram()
 
-mean(future_RSI_mid$forward[!is.na(future_RSI_mid$forward)])
-mean(future_RSI_low$forward[!is.na(future_RSI_low$forward)])
+median(future_RSI_mid$forward[!is.na(future_RSI_mid$forward)])
+median(future_RSI_low$forward[!is.na(future_RSI_low$forward)])
+median(future_RSI_high$forward[!is.na(future_RSI_high$forward)])
 
 
